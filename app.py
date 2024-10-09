daba2016fe0c13d0f042acfd9bac3978043c7774
Arnav11n <91536262+Arnav11n@users.noreply.github.com>
2024-10-09 22:15:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ def get_data(latitude, longitude):
         'average_ndvi': avgNDVI.get('NDVI').getInfo()
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
